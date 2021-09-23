986556f3c696a4001bcb0d1d88845f91b7fb6240
Abid Rahman <abid@opn.ooo>
2021-09-23 13:54:03 +0600

    change

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
             targets: ["FrameworkA"]),
     ],
     dependencies: [
-        .package(url: "git@github.com:abid-opn/FrameworkB.git", from: "0.1.0")
+        .package(name: "FrameworkB", url: "git@github.com:abid-opn/FrameworkB.git", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FrameworkA",
-            dependencies: [])
+            dependencies: ["FrameworkB"])
     ]
 )
